bc6dfbfeb0a87bd50a52631a4c0e4bf3082509de
Aymen abdellaoui <aymen.abdellaoui997@gmail.com>
2024-07-11 13:31:58 +0200

    chore: add useless stuff

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 #! /usr/bin/env node
 const { program, Option } = require("commander");
-const { proxyTCP, proxyUDP } = require("./serv");
+const udp = require('dgram');
+const net = require('net');
+const chalk = require('chalk');
 
+const proxyTCP = ({ from, to }) => {
+  const server = net.createServer((socket) => {
+    const client = net.createConnection({ port: to }, () => {
+      const msg = chalk.green(` ${socket.remoteAddress}:${socket.remotePort} -> ${to}`);
+      console.log(chalk.green.bold(`[INFO]`), `New connection: ${msg}`);
+      socket.pipe(client);
+      client.pipe(socket);
+    });
+  });
+  server.on('error', (err) => {
+    const msg = chalk.red(err.message);
+    console.error(chalk.red.bold(`[ERROR]`), `Error: ${msg}`);
+    process.exit(1);
+  }
+  );
+  server.listen(from);
+  console.log(chalk.green.bold(`[INFO]`), `Proxying from ${from} to ${to}`);
+}
 
+const proxyUDP = ({ from, to }) => {
+  const server = udp.createSocket('udp4');
+  server.on('message', (msg, info) => {
+    const client = udp.createSocket('udp4');
+    client.send(msg, to, 'localhost', (err) => {
+      if (err) {
+        console.log(chalk.red.bold(`[ERROR]`), `Error: ${chalk.red(err.message)}`);
+        process.exit(1);
+      }
+      const message = chalk.green(` ${info.address}:${info.port} -> ${to}`);
+      console.log(chalk.green.bold(`[INFO]`), `New message: ${message}`);
+    });
+  });
+  server.bind(from);
+}
 
 program
   .name('port-forwarder')
@@ -16,7 +51,7 @@ program
 
 program.parse(process.argv)
 const options = program.opts()
-console.log(options)
+
 
 if (options.protocol === 'tcp') {
   proxyTCP(options)
